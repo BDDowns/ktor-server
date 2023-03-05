d4cf54a40ec2b0417a7bc444c098866fadc89468
Bryan Downs <BDDowns@users.noreply.github.com>
2023-03-05 05:43:47 -0700

    update db config

--- src/main/kotlin/DB.kt
+++ src/main/kotlin/DB.kt
@@ -3,9 +3,9 @@ import org.jetbrains.exposed.sql.Database
 object DB {
     private val host = System.getenv("DB_HOST") ?: "localhost"
     private val port = System.getenv("DB_PORT")?.toIntOrNull() ?: 5432
-    private val dbName = System.getenv("DB_NAME") ?: "ktor_db"
-    private val dbUser = System.getenv("DB_USER") ?: "ktor_admin"
-    private val dbPassword = System.getenv("DB_PASSWORD") ?: "ktor"
+    private val dbName = System.getenv("DB_NAME") ?: "users_db"
+    private val dbUser = System.getenv("DB_USER") ?: "users_admin"
+    private val dbPassword = System.getenv("DB_PASSWORD") ?: "users"
 
     fun connect() =
         Database.connect(
